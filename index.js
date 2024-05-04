6803aebe08278a0ce479372c5d26074a9a1a2453
muhammaddaffafauzan <mobileasia215@gmail.com>
2024-05-04 09:17:27 +0700

    update

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 var express = require("express");
 var cors = require("cors");
 var multer = require("multer");
-var upload = multer({ dest: "uploads/" });
+var path = require("path");
 require("dotenv").config();
 
 var app = express();
 
+// Middleware
 app.use(cors());
 app.use("/public", express.static(process.cwd() + "/public"));
 
+// Multer configuration
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // Simpan file di dalam direktori "uploads/"
+  },
+  filename: function (req, file, cb) {
+    // Ubah nama file yang disimpan sesuai dengan nama asli file yang diunggah
+    cb(null, file.originalname);
+  },
+});
+
+var upload = multer({ storage: storage });
+
+// Routes
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
@@ -26,6 +41,7 @@ app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
   });
 });
 
+// Server setup
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Your app is listening on port " + port);
